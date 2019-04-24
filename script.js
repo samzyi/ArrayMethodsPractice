d9292134df824d209a9fcf3314fa691f0144d3f5
samzyi <46505136+samzyi@users.noreply.github.com>
2019-04-24 10:54:34 -1000

    project

---
+++ script.js
@@ -0,0 +1,19 @@
+var grateful = ["job","boyfriend"];
+grateful.pop();
+console.log(grateful)
+grateful.push("house","parents")
+console.log(grateful)
+grateful.shift();
+console.log(grateful);
+grateful.unshift("puppy");
+console.log(grateful);
+
+var proud = ["project a","project b"];
+proud.pop();
+console.log(proud)
+proud.push("project c","project d")
+console.log(proud)
+proud.shift();
+console.log(proud);
+proud.unshift("project e");
+console.log(proud);
